f32d15fd5ee772baaa76a844e972f5db1bf17e7b
sehyeong <sehyeong@sehyeongui-MacBookAir.local>
2021-04-15 15:31:58 +0900

    Modify: DB 연결에 pymysql 사용, class형 view 예시 작성 (진행중)

--- backend/store/app.py
+++ backend/store/app.py
@@ -1,21 +1,16 @@
-
 import config
 
-from flask import Flask 
+from flask import Flask
 from flask_cors import CORS
 from sqlalchemy import create_engine, text
+
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
     if test_config is None:
-        app.config.from_pyfile('config.py')
+        app.config.from_pyfile("config.py")
     else:
         app.config.update(test_config)
 
-    database  = create_engine(app.config['DB_URL'], encoding = 'utf-8', max_overflow = 0)
-    app.database = database
-    
     return app
-
-
