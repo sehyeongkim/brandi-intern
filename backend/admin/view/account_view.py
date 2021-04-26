6e6ab1dbde47af486b02ba4dc8be33d4338be7df
ByungSoo Ye <ybs680318@gmail.com>
2021-04-27 00:28:48 +0900

    [Admin > order] - order_service > order_list, order key값 변경 - 주문 상태 변경 함수 수정 	- order_dao에서 status_type 존재 여부 확인 	- order_dao에서 변경 가능 여부 확인

--- backend/admin/view/account_view.py
+++ backend/admin/view/account_view.py
@@ -70,4 +70,7 @@ class AccountLogInView(MethodView):
                  ), 200
         
         finally:
-            conn.close()
+            try:
+                conn.close()
+            except Exception as e: 
+                raise DatabaseCloseFail('서버에 알 수 없는 오류가 발생했습니다.')
--- backend/utils/response.py
+++ backend/utils/response.py
@@ -28,10 +28,10 @@ def get_response(result, status_code=200):
                 }
     return response
 
-def post_response_with_return(message, *args, status_code=200):
+def post_response_with_return(message, fail_return, status_code=200):
     response = {
                     "message": message,
-                    "post_fail": args,
+                    "post_fail": fail_return,
                     "status_code": status_code
                 }
     return response
